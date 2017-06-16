26958689c0ca9f5be34ccefa9c6eedf7571505b7
周馥铭 <18219116517@163.com>
2017-06-16 15:02:52 +0800

    html5 webpage five commit

--- js/index.js
+++ js/index.js
@@ -238,4 +238,14 @@ $(function(){
 			scroll.style.left =  left +"px";
 		}
 	);
+
+	/* 移动设备横竖屏 */
+	setRem();
+	window.addEventListener("orientationchange", setRem);
+	window.addEventListener("resize", setRem);
+	function setRem() {
+		var html = document.querySelector("html");
+		var width = html.getBoundingClientRect().width;
+		html.style.fontSize = width / 16 + "px";
+	}
 });
